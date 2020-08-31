ce6e3785fd41478929358408d0f34f78c41a0998
fizikaislove <30771924+fizikaislove@users.noreply.github.com>
2020-08-31 14:19:31 +0300

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,17 @@ import sphinx_rtd_theme
 
 from datetime import datetime
 sys.path.insert(0, os.path.abspath('../../../'))
+print("this is the path ",os.path.abspath('../../../../'))
+print('another path: ',os.listdir(os.path.abspath('../../../../')))
 print("this is the path ",os.path.abspath('../../../'))
 print('another path: ',os.listdir(os.path.abspath('../../../')))
+print("this is the path ",os.path.abspath('../../'))
+print('another path: ',os.listdir(os.path.abspath('../../')))
+print("this is the path ",os.path.abspath('../'))
+print('another path: ',os.listdir(os.path.abspath('../')))
+# print("this is the path ",os.path.abspath('../../../'))
+# print('another path: ',os.listdir(os.path.abspath('../../../')))
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
