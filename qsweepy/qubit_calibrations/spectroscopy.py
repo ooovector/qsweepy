4618075628b00f74aa7d31ed09af2fc746b514fe
GrigorevSasha <grigorev.sasha8008@gmail.com>
2020-10-15 10:50:28 +0300

    Added way to set some spectroscopy parameters externally

--- qsweepy/qubit_calibrations/spectroscopy.py
+++ qsweepy/qubit_calibrations/spectroscopy.py
@@ -1,4 +1,4 @@
-from qsweepy.fitters import fit_dataset, resonator_tools
+from qsweepy.fitters import fit_dataset, resonator_tools, spectroscopy_overview
 import numpy as np
 import time
 
@@ -19,10 +19,24 @@ def single_tone_spectroscopy_overview(device, fmin, fmax, nop, *args):
         #device.hardware.pna.measure()
         #time.sleep(device.hardware.pna.get_sweep_time()*0.001)
         device.hardware.pna.measure()
-        device.sweeper.sweep(device.hardware.pna, *args,
+        result = device.sweeper.sweep(device.hardware.pna, *args,
                                measurement_type='single_tone_spectroscopy_overview',
                                metadata={ 'vna_power': device.hardware.pna.get_power(),
                                           'bandwidth': bandwidth})
+
+        fitter = spectroscopy_overview.SingleToneSpectroscopyOverviewFitter()
+
+        fit_dataset.fit_dataset_1d(
+            source_measurement=result,
+            dataset_name='S-parameter',
+            fitter=fitter,
+            time_parameter_id=-1,
+            sweep_parameter_ids=np.arange(len(args)),
+            allow_unpack_complex=False,
+            use_resample_x_fit=False)
+
+        device.exdir_db.save_measurement(result.fit)
+
     except:
         raise
 
@@ -83,18 +97,21 @@ def measure_fr(device, qubit_id, fr_guess):
     return result.fit
 
 
-def two_tone_spectroscopy(device, qubit_id, fq_guess, *args, power_excite=None, power_readout=None):
+def two_tone_spectroscopy(device, qubit_id, fq_guess, *args, power_excite=None, power_readout=None, span=None, nop=None, bandwidth=None):
     try:
         device.hardware.set_cw_mode()
         device.hardware.lo1.set_status(1)
 
-        span = float(device.get_qubit_constant(qubit_id=qubit_id, name='two_tone_spectrum_span'))
+        if span is None:
+            span = float(device.get_qubit_constant(qubit_id=qubit_id, name='two_tone_spectrum_span'))
         if power_excite is None:
             power_excite = float(device.get_qubit_constant(qubit_id=qubit_id, name='two_tone_spectrum_excite_power'))
         if power_readout is None:
             power_readout = float(device.get_qubit_constant(qubit_id=qubit_id, name='two_tone_spectrum_readout_power'))
-        nop = int(device.get_qubit_constant(qubit_id=qubit_id, name='two_tone_spectrum_nop'))
-        bandwidth = float(device.get_qubit_constant(qubit_id=qubit_id, name='two_tone_spectrum_bandwidth'))
+        if nop is None:
+            nop = int(device.get_qubit_constant(qubit_id=qubit_id, name='two_tone_spectrum_nop'))
+        if bandwidth is None:
+            bandwidth = float(device.get_qubit_constant(qubit_id=qubit_id, name='two_tone_spectrum_bandwidth'))
         fr = float(device.get_qubit_fr(qubit_id=qubit_id))
 
 
