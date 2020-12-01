a2b3035a792ca0bfb26198ff760d8ea6baf50b1e
Ilya Besedin <gamlo@blincake.org>
2020-12-01 13:33:57 +0300

    T1 flux scan is added

--- qsweepy/qubit_calibrations/zgate.py
+++ qsweepy/qubit_calibrations/zgate.py
@@ -1,6 +1,7 @@
 from qsweepy.qubit_calibrations import excitation_pulse
-from qsweepy.qubit_calibrations import Ramsey
+from qsweepy.qubit_calibrations import Ramsey, relaxation
 from qsweepy.qubit_calibrations import channel_amplitudes
+
 
 def zgate_ramsey(device, gate):
     def filler_func(length):
@@ -16,7 +17,7 @@ def zgate_ramsey(device, gate):
                            delay_seq_generator=filler_func, measurement_type='Ramsey_long_process', additional_references={'gate':gate.id})
 
 
-def zgate_amplitude_ramsey(device, gate, lengths, amplitudes, target_freq_offset=100e69):
+def zgate_amplitude_ramsey(device, gate, lengths, amplitudes, target_freq_offset=100e9):
     pre_pause = float(gate.metadata['pre_pause'])
     post_pause = float(gate.metadata['post_pause'])
     class ParameterSetter:
@@ -39,4 +40,30 @@ def zgate_amplitude_ramsey(device, gate, lengths, amplitudes, target_freq_offset
     return Ramsey.Ramsey(device, gate.metadata['target_qubit_id'], '01', (amplitudes, setter.amplitude_setter, 'amplitude'),
                            lengths = lengths, target_freq_offset=target_freq_offset,
                            delay_seq_generator=setter.filler_func, measurement_type='Ramsey_amplitude_scan',
+                           additional_references={'gate':gate.id})
+
+
+def zgate_amplitude_relaxation(device, gate, lengths, amplitudes):
+    pre_pause = float(gate.metadata['pre_pause'])
+    post_pause = float(gate.metadata['post_pause'])
+    class ParameterSetter:
+        def __init__(self):
+            self.amplitude=None
+
+        def amplitude_setter(self, amplitude):
+            self.amplitude = amplitude
+
+        def filler_func(self, length):
+            channel_amplitudes_ = channel_amplitudes.channel_amplitudes(device,
+                                                  **{gate.metadata['carrier_name']: self.amplitude})
+            return [device.pg.pmulti(pre_pause)]+excitation_pulse.get_rect_cos_pulse_sequence(device = device,
+                                               channel_amplitudes = channel_amplitudes_ ,
+                                               tail_length = float(gate.metadata['tail_length']),
+                                               length = length,
+                                               phase = 0.0)+[device.pg.pmulti(post_pause)]
+    setter = ParameterSetter()
+
+    return relaxation.relaxation(device, gate.metadata['target_qubit_id'], '01', (amplitudes, setter.amplitude_setter, 'amplitude'),
+                           lengths = lengths,
+                           delay_seq_generator=setter.filler_func, measurement_type='relaxation_amplitude_scan',
                            additional_references={'gate':gate.id})
